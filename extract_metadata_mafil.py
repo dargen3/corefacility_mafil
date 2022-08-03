5dfaa41bda4388b87e1974928b0f9f9ee52a2b09
dargen3 <dargen3@centrum.cz>
2022-08-03 11:13:23 +0200

    more readable

--- extract_metadata_mafil.py
+++ extract_metadata_mafil.py
@@ -13,13 +13,8 @@ def load_arguments():
     return args
 
 
-def load_files(dataset_description_file):
-    dataset_description = json.loads(open(dataset_description_file, "r").read())
+def validate_metadata(metadata):
     json_scheme = json.loads(open("scheme_mafil.json", "r").read())
-    return dataset_description, json_scheme
-
-
-def validate_metadata(metadata, json_scheme):
     validator = jsonschema.Draft202012Validator(json_scheme)
     errors = sorted(validator.iter_errors(metadata))
     if not errors:
@@ -34,5 +29,5 @@ def validate_metadata(metadata, json_scheme):
 
 if __name__ == "__main__":
     args = load_arguments()
-    dataset_description, json_scheme = load_files(args.dataset_description)
-    validate_metadata(dataset_description, json_scheme)
+    metadata = json.loads(open(args.dataset_description, "r").read())
+    validate_metadata(metadata, json_scheme)
